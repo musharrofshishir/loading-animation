8035464be78df7a10f32f8b0e63cecd83251ad92
Musharrof Shishir <musharrofshishir@users.noreply.github.com>
2020-11-01 15:42:05 +0600

    updated to previous

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,14 @@
-    var g = gsap;
-    var tl = new TimelineMax({repeat:-1, yoyo: true, repeatDelay:.4});
-
-    tl.from("#group", {duration: .2, y:-75, transformOrigin: "50% 50%", opacity:0, ease: Bounce.easeOut})
-    .to("#path4", {duration: .15, y:"-=30", skewY:"+=10"})
-    .to("#path4", {duration: .15, y:"+=30", skewY:"-=10"});
-        
-    // tl.to("#group", {opacity: 0});    
-    
-
+var tl = gsap;
+tl.from("#path1", {duration: 1, x: 100, y: 100, scale: 0.5, rotation: 180, skewX: 45});
+// tl.from("#path1", {duration: 1, x: 50, y: 40, rotation: 55});
+tl.from("#path2", {duration: 1, x: 100, y: 100, scale: 0.5, rotation: 180, skewX: 45});
+// tl.from("#path2", {duration: 1, x: -50, y: 120, rotation: -55});
+tl.from("#path3", {duration: 1, x: 100, y: 100, scale: 0.5, rotation: 180, skewX: 45});
+// tl.from("#path3", {duration: 1, x: 50, y: 120, rotation: 55});
+tl.from("#path4", {duration: 1, x: 100, y: 100, scale: 0.5, rotation: 180, skewX: 45});
+// tl.from("#path4", {duration: 1, x: -50, y: 110, rotation: -55});
+tl.from("#path5", {duration: 1, x: 100, y: 100, scale: 0.5, rotation: 180, skewX: 45});
+// tl.from("#path5", {duration: 1, x: 50, y: 220, rotation: 55});
+tl.to("#group", {duration: 2, rotation: 360, transformOrigin: "50% 50%"});
 
 // transform: translate(-50px, -40px) rotate(-55deg);
